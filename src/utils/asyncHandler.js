653dfb960eaa87d2a6f526c48933acbfc430afb3
Dheeraj Patel <pateldheeraj888@gmail.com>
2025-03-19 17:14:53 +0530

    updating asynhandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = async (requestHandler) => {
-    (req,res,next)=>{
+   return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
     }
 }
